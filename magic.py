1edaf7d0721ac247dbb9c54f00f94facdfef6de7
vidusheeamoli <vidushee.amoli@gmail.com>
2019-02-17 05:21:03 +0530

    ugly, but it works :'D

--- magic.py
+++ magic.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import os
 
-
+character_box = ['.',',',':',';','+','*','?','%','S','#','@']
 
 def resize(image):
 	(old_width, old_height) = image.size
 	aspect_ratio = (1.0*old_height)/(1.0*old_width)
-	new_height = aspect_ratio*(500)
-	new_size = (500, new_height)
+	new_height = aspect_ratio*(100)
+	new_size = (100, new_height)
 	image = image.thumbnail(new_size)
 	#return image
 
@@ -15,6 +15,12 @@ def resize(image):
 def black_white(image):
 	image=image.convert(mode="L")
 	return image
+
+
+def pixel_to_ascii(image):
+	pixel_info = list(image.getdata())
+	pixels_to_chars = [character_box[pixel_value/25] for pixel_value in pixel_info]
+	return "".join(pixels_to_chars)
 
 
 # for f in os.listdir('.'):
@@ -25,6 +31,7 @@ def black_white(image):
 # 		print fext 
 # 		i.thumbnail(size)
 # 		i.save('new.jpg')
+
 
 
 ''' Step 1: Resize but keep the aspect ratio same because you dont want to squish your image '''
@@ -58,9 +65,21 @@ if __name__ == '__main__':
 		#Black&White
 		image=black_white(image)
 
-		image.show()
+		pixels_to_chars = pixel_to_ascii(image)
+
+		len_pixels_to_chars = len(pixels_to_chars)
+
+		image_ascii = [pixels_to_chars[index: index + 100] for index in xrange(0, len_pixels_to_chars, 100)]
+
+		final_ascii_image = "\n".join(image_ascii)
+
+		print final_ascii_image
+
 	
 	else:
 		print (" File not found :( ")
+
+
+
 
 
