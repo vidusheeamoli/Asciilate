d6f7c9127734b9bd39d02351a525cdffe8291c88
vidusheeamoli <vidushee.amoli@gmail.com>
2019-02-17 02:38:36 +0530

    Resize works

--- magic.py
+++ magic.py
@@ -1,7 +1,19 @@
 from PIL import Image
+import os
 
 #creating image object
+#image1 = Image.open('img.jpg')
 
-image1 = Image.open('img.jpg')
-image1.show()
-image1.save('img1.png')
+''' Step 1: Resize but keep the aspect ratio same because you dont want to squish your image '''
+
+size= (5000,50)
+
+for f in os.listdir('.'):
+	if f.endswith('.jpg'):
+		i=Image.open(f)
+		fn, fext = os.path.splitext(f)
+		''' print fn
+		print fext '''
+		i.thumbnail(size)
+		i.save('new.jpg')
+ 
